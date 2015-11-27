57388e1805fa0a5bef79d25464bd942f0dfbfe75
xxaxxelxx <x@axxel.net>
2015-11-27 10:31:24 +0100

    updated

--- listeners.php
+++ listeners.php
@@ -8,19 +8,18 @@ error_reporting(E_ALL);
 
 $db = new SQLite3('load.db');
 $results = $db->query("SELECT sum(listeners) FROM t_pool where mountpoint like '%$mountpoint%'");
+
+# eliminating intro mountpoint self listening
 $intromounts = $db->query("SELECT listeners FROM t_pool where mountpoint like '/intro%$mountpoint%'");
-$numberof_intromounts = $intromounts->numRows();
+$numberof_intromounts = 0; while ( $row = $intromounts->fetchArray() ) { $numberof_intromounts++; };
+
 while ( $row = $results->fetchArray() ) {
     if (empty($row[0])) {
 	    echo "0\n";
     } else {
 	    $number = $row[0] - $numberof_intromounts;
-	    echo $row[0]."\n";
-	    echo $numberof_intromounts."\n";
 	    echo $number."\n";
     }
 };
 $db->close();
-#echo "<hr>OK!<br>\n";
-
 ?>
