717d78d22cb5f77f7dfbf014bc92b7322b137edb
xxaxxelxx <x@axxel.net>
2015-12-17 09:24:57 +0100

    updated

--- listmachines_human.php
+++ listmachines_human.php
@@ -7,14 +7,19 @@ init_db();
 error_reporting(E_ALL);
 
 $db = new SQLite3('load.db');
-$results = $db->query("SELECT machineip,bandwidth,bandwidthlimit,load FROM t_pool group by machineip");
+$results = $db->query("SELECT machineip,bandwidth,bandwidthlimit,load,timestamp FROM t_pool group by machineip");
 
+echo "<table>";
+echo "<tr><th>TSTAMP</th><th>IP</th><th>BW</th><th>BWLIMIT</th><th>LOAD</th></tr>\n";
 while ( $row = $results->fetchArray() ) {
-   echo $row['timestamp']."|";
-   echo $row['machineip']."|";
-   echo $row['bandwidth']."|";
-   echo $row['bandwidthlimit']."|";
-   echo $row['load']."<br>\n";
+    echo "<tr>";
+    echo "<td>".$row['timestamp']."</td>";
+    echo "<td>".$row['machineip']."</td>";
+    echo "<td>".$row['bandwidth']."</td>";
+    echo "<td>".$row['bandwidthlimit']."</td>";
+    echo "<td>".$row['load']."</td>\n";
+    echo "</tr>";
 }
+echo "</table>";
 $db->close();
 ?>
