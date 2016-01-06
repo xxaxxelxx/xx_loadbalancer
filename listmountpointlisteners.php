34527bd45c77545d04eff219189f632b39a646ff
xxaxxelxx <x@axxel.net>
2016-01-06 11:48:03 +0100

    updated

--- listmountpointlisteners.php
+++ listmountpointlisteners.php
@@ -13,21 +13,23 @@ while ( $row = $results->fetchArray() ) {
     $listsum = 0;
     $mountpoint = ltrim ($row['mountpoint'], '/');
 
-    $results2 = $db->query("SELECT sum(listeners) FROM t_pool where mountpoint like '/$mountpoint'");
+    $results2 = $db->query("SELECT sum(listeners) FROM t_pool where mountpoint like '%$mountpoint'");
 	while ( $row2 = $results2->fetchArray() ) {
-	    $listsum_basic = $row2[0];
+	    $listsum = $row2[0];
 	}
-    $results3 = $db->query("SELECT sum(listeners) FROM t_pool where mountpoint like '/intro.$mountpoint'");
-	while ( $row3 = $results3->fetchArray() ) {
-	    $listsum_intro = $row3[0];
-	}
+#    $results3 = $db->query("SELECT sum(listeners) FROM t_pool where mountpoint like '/intro.$mountpoint'");
+#	while ( $row3 = $results3->fetchArray() ) {
+#	    $listsum_intro = $row3[0];
+#	}
 
     # eliminating intro mountpoint self listening
 #    $intromounts = $db->query("SELECT listeners FROM t_pool where mountpoint like '/intro.$mountpoint'");
 #    $numberof_intromounts = 0; while ( $row = $intromounts->fetchArray() ) { $numberof_intromounts++; };
 
 #    $listsum = $listsum_basic + $listsum_intro - $numberof_intromounts;
-    $listsum = $listsum_basic + $listsum_intro;
+#    $listsum = $listsum_basic + $listsum_intro;
+#    $listsum = $listsum_basic;
+
     if ( $listsum < 0 ) { $listsum = 0; };
 
     echo $listsum."@".$mountpoint."\n";
