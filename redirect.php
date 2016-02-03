616064d121d9d0a2f63e3c87fc0643e65ca8846f
xxaxxelxx <x@axxel.net>
2016-02-03 14:08:28 +0100

    updated

--- redirect.php
+++ redirect.php
@@ -49,8 +49,8 @@ error_reporting(E_ALL);
 $maxload = 75;
 
 # within which time in seconds clients will stay in pool
-#$maxage = $((3600 * 24 * 31));
-$maxage = 3600;
+$maxage = 3600 * 24 * 31;
+#$maxage = 3600;
 $maxagelimitstamp = $tstampnow - $maxage; 
 cleanup_listeners($maxagelimitstamp);
 
